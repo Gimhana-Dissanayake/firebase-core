86c9cd03b905daa0fe8a1cd9467cfc6926c5f8be
Gimhana <gimhanadev@gmail.com>
2021-11-30 09:04:56 +0530

    silence the warning

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+// eslint-disable-next-line no-unused-vars
+import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
